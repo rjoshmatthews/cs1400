48fd6d289d36d05d5cdee47a8065704920d08ff8
Robert Matthews <rmatthews@gmail.com>
2018-03-16 11:09:39 -0600

    removed magic numbers

--- practice/table_loop.py
+++ practice/table_loop.py
@@ -22,14 +22,17 @@ total = 2
 parents = 2
 month = 0
 babies = 0
+babies_per_parent = 2
+cages = 500
+parents_per_cage = 2
 
-while int(total/2) < 500:
+while int(total) < cages * parents_per_cage:
     month += 1
     # promote month old babies to parents.
     parents += babies
 
     # calculate new babies from parents
-    babies = int(parents/2)*2
+    babies = int(parents/2) * babies_per_parent
     total = parents + babies
 
     print(month, "\t\t", parents, "\t\t", babies, "\t\t", total)
